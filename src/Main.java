a1429b17c0fb76516364e804f6866f09cf4333d0
LufatZ <lucasdamme65@gmail.com>
2024-02-18 01:48:11 +0100

    Ändern der Integer zu floats um Gleitkommazahlen zu ermöglichen.
    Separierung der Berechnung in eigene Methode
    
    Nutzen einer Array-Liste für die eingegeben nummern
    
    Bessere Überprüfung der Eingabe des Operators
    Es können jetzt mehr als zwei zahlen in die formel eingegeben werden

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         for (boolean running = true; running;) {
-            System.out.println("Welche Operation wollen Sie Ausführen?");
+            System.out.println("Gib eine arithmetische Operation ein oder (*,+,/,-):");
             String operation = scanner.nextLine();
-            System.out.println("Bitte gib die erste Zahl an");
-            int first = scanner.nextInt();
-            System.out.println("Bitte gib die zweite zahl an");
-            int scnd = scanner.nextInt();
-            int result = 0;
-            if (operation.equals("*") || operation.contains("mult")) {
-                result = first * scnd;
-            }
-            if (operation.equals("+") || operation.contains("add")) {
-                result = first + scnd;
-            }
-            if (operation.equals("-") || operation.contains("sub")) {
-                result = first - scnd;
-            }
-            if (operation.equals("/") || operation.contains("div")) {
-                result = first / scnd;
+            if (containsOperator(operation)) {
+                List<Float> numbers =new ArrayList<>();
+                System.out.println("Bitte gib die erste Zahl an");
+                numbers.add(scanner.nextFloat());
+                System.out.println("Bitte gib die zweite zahl an");
+                numbers.add(scanner.nextFloat());
+                scanner.nextLine();
+                while (true){
+                    System.out.println("Gib eine weitere Zahl an oder result (r) um das Ergebniss anzuzeigen");
+                    String input = scanner.nextLine();
+                    if (input.contains("r")){
+                        break;
+                    }
+                    numbers.add(Float.parseFloat(input));
+                }
+                System.out.println("Das Ergebniss ist: " + berechnung(operation,numbers));
             } else {
-                System.out.println("Operator" + operation + "nicht gefunden");
+                System.out.println("Operator nicht gefunden");
             }
-            System.out.println("Das Ergebniss ist: " + result);
             System.out.println("soll eine weitere Operation ausgeführt werden? Antworte mit true oder flase");
             String continueResponse = scanner.next();
             running = continueResponse.equalsIgnoreCase("true");
             scanner.nextLine(); // Consume newline character
         }
         scanner.close(); // Close scanner to prevent resource leak
+    }
+    public static boolean containsOperator(String operation){
+        String[] operators = {"+","-","/","*","m","d","s","a"};
+        for (int i = 0; i < operators.length; i++) {
+            if(operation.contains(operators[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static float berechnung(String operation, List<Float> numbers){
+        float result = numbers.get(0);
+        for (int i = 1; i < numbers.size(); i++) {
+            if (operation.equals("*") || operation.contains("m")) {
+                result *= numbers.get(i);
+            }
+            if (operation.equals("+") || operation.contains("a")) {
+                result += numbers.get(i);
+            }
+            if (operation.equals("-") || operation.contains("s")) {
+                result -= numbers.get(i);
+            }
+            if (operation.equals("/") || operation.contains("d")) {
+                if (numbers.contains(0F)){
+                    System.out.println("Teilen durch null (0) ist nicht erlaubt");
+                }else {
+                    result /= numbers.get(i);
+                }
+            }
+        }
+        return result;
     }
 }
